7cb17f7ed055ce92980452d9263b9c585a6868c1
oniel1239 <oliverkcw199@gmail.com>
2026-07-20 14:43:42 +0500

    Fix: remove unused useCallback import to fix TS build error

--- src/sections/Portfolio.tsx
+++ src/sections/Portfolio.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { asset } from '@/lib/utils';
